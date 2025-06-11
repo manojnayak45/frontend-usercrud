e8411fb38250a2aa31bf608203ceaa6c18e9cc9c
Rathod Manoj Kumar <rathodmanoj2022@gmail.com>
2025-06-11 22:49:07 +0530

    resolved

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,19 +1,9 @@
-// utils/axios.js
+// src/utils/axios.js
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://backend-crud-ltwb.onrender.com/api",
   withCredentials: true,
 });
-
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default instance;
