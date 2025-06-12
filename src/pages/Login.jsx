967bde8881c2d985f935ebd639416e81d59c14c7
Rathod Manoj Kumar <rathodmanoj2022@gmail.com>
2025-06-12 10:30:36 +0530

    resolved the slash which is entereted by mistake

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-\import React, { useState } from "react";
+import React, { useState } from "react";
 import axios from "../utils/axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
